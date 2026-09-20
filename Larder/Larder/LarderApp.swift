a2663a2ee936b0962578ee64bdc16e9783b5ff12
The Quantum Dev <thequantumdev@gmail.com>
2026-09-20 15:25:50 -0400

    Add Xcode project
    SwiftUI app target with unit and UI test targets, iOS 26.5 deployment target.

---
+++ Larder/Larder/LarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarderApp.swift
+//  Larder
+//
+//  Created by Joshua Samuel on 9/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Larder/LarderTests/LarderTests.swift
@@ -0,0 +1,19 @@
+//
+//  LarderTests.swift
+//  LarderTests
+//
+//  Created by Joshua Samuel on 9/20/26.
+//
+
+import Testing
+@testable import Larder
+
+struct LarderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
